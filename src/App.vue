f51922a931df85055810214241588fc032d488da
Jeremy Hale <jeremy.hale@gmail.com>
2024-04-10 08:24:10 +0900

    Add sign in page

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/signin">Sign In</RouterLink>
       </nav>
     </div>
   </header>
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
-    <input />
-    <input />
-    <button>Sign In</button>
+    <input v-model="email" placeholder="email" />
+    <input v-model="password" placeholder="password" />
+    <button @click="doSignIn()">Sign In</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useFirebaseAuth } from "vuefire";
+import { signInWithEmailAndPassword, type Auth } from "firebase/auth";
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+const auth = useFirebaseAuth();
+
+function doSignIn() {
+  signInWithEmailAndPassword(auth as Auth, email.value, password.value)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+}
+</script>
 
 <style scoped></style>
